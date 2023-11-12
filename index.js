bab95eda1c71abcf19b004dcc13269b1c7fec7f6
nicmackenzie <nicmackenzie@live.com>
2023-11-12 12:10:48 +0300

    error logging

--- index.js
+++ index.js
@@ -5,9 +5,32 @@ dotenv.config({ path: './.env' });
 const PORT = process.env.PORT || 8000;
 
 /* eslint-disable */
+// uncaught exceptions
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 server.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `);
 });
-/* eslint-enable */
 
-// Export the Express API
+// handle unhandled rejections
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+// HANDLE SIGTERM FROM HEROKU
+process.on('SIGTERM', () => {
+  console.log('✋ SIGTERM RECEIVED: Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 process terminated');
+  });
+});
+
+/* eslint-enable */
