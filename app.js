4bdbfe51d12050576571353bbac9f8297479926b
nicmackenzie <nicmackenzie@live.com>
2023-11-12 12:10:15 +0300

    rate limit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const hpp = require('hpp');
 const compression = require('compression');
+const rateLimit = require('express-rate-limit');
 const AppError = require('./utils/AppError');
 
 const app = express();
@@ -12,6 +13,15 @@ app.options('*', cors());
 
 // set http security headers
 app.use(helmet());
+
+if (process.env.NODE_ENV === 'production') {
+  const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!',
+  });
+  app.use('/', limiter);
+}
 
 app.use(express.json());
 
